410cabbcdc2e7bf3548ff546c5456337d797dcbe
adharbert <adharbert@gmail.com>
2019-04-22 08:24:02 -0400

    adding redux

--- react/seasons/src/index.js
+++ react/seasons/src/index.js
@@ -1,51 +1,20 @@
-
-import React, { Component } from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
-// state must be initialzed when a compenent is created
-// state can only be updated using the function setState();
-class App extends Component {
-    state = { lat: null, errorMessage: '' };
+import App from './components/App';
+import reducers from './reducers';
 
-
-    componentDidMount() {
-        console.log('My component was rendered to the screen');        
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ lat: position.coords.latitude }), 
-            (err) => this.setState({ errorMessage: err.message })
-        );
-    }
-
-    componentDidUpdate() {
-        console.log('My component was just updated - it rerendered!');
-    }
-
-    contentRendered() {
-        let displayItem = <Spinner text="Please accept location request" />;
-        if (this.state.errorMessage && !this.state.lat) {
-            displayItem = <div>Error: {this.state.errorMessage}</div> ;
-        } else if (!this.state.errorMessage && this.state.lat) {
-            displayItem = <SeasonDisplay lat={this.state.lat} />
-        }
-        return displayItem;
-    };
-
-    // React says we have to define render
-    render() {
-
-        
-
-        return (
-            <div className="border red">
-                {this.contentRendered()}
-            </div>
-        );
-    }
-}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware())
+);
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 );
